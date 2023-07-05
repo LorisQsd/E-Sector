eedd8668000931106cb14f1366ba666e6d77043a
Loris QUESADO <loris.quesado@oclock.school>
2023-07-05 13:00:06 +0200

    Réorganisation de la database et des méthodes de recherche : fetch + filter

--- app.js
+++ app.js
@@ -1,96 +1,88 @@
-//RADIO BUTTONS
+// Sélecteurs
 const labels = document.querySelectorAll(".radio-group label");
 const inputs = document.querySelectorAll(".radio-group input");
-let dataArray = goussainville;
-let city = "Goussainville";
+//SEARCH INPUT
+const searchInput = document.querySelector("#search");
+const tbody = document.querySelector("tbody");
+const thead = document.querySelector("thead");
+const table = document.querySelector("table");
+const resultMsg = document.querySelector("#search-result");
+
+// Variables
+let city = "goussainville";
 
 labels.forEach(label => {
-    label.addEventListener("click", handleSector)
+    label.addEventListener("click", handleSector);
 
-    function handleSector(e) {
+    function handleSector() {
         inputs.forEach(input => {
             setTimeout(() => {
 
                 if (input.checked) {
                     if (label.getAttribute("id") === "fontenay") {
-                        dataArray = fontenay
-                        city = "Fontenay-en-Parisis"
+                        city = "fontenay";
                     } else if (label.getAttribute("id") === "gouss") {
-                        dataArray = goussainville
-                        city = "Goussainville"
+                        city = "goussainville";
                     } else if (label.getAttribute("id") === "thillay") {
-                        dataArray = thillay
-                        city = "Le Thillay"
+                        city = "thillay";
                     } else if (label.getAttribute("id") === "louvres") {
-                        dataArray = louvres
-                        city = "Louvres"
+                        city = "louvres";
                     }
-                    const labelIndex = input.getAttribute("data-index")
-                    labels.forEach(label => { label.style.background = "none"; label.style.color = "black" })
-                    labels[labelIndex].style.background = "linear-gradient(to right, #0f0c29, #302b63, #24243e)"
-                    labels[labelIndex].style.color = "white"
+                    const labelIndex = input.getAttribute("data-index");
+                    labels.forEach(label => { label.style.background = "none"; label.style.color = "black"; });
+                    labels[labelIndex].style.background = "linear-gradient(to right, #0f0c29, #302b63, #24243e)";
+                    labels[labelIndex].style.color = "white";
 
-                    filterData();
+                    if(searchInput.value.length >= 3) {
+                        filterData(city);
+                    } else {
+                        thead.textContent = "";
+                        tbody.textContent = "";
+                        resultMsg.textContent = "";
+                        table.style.opacity = "0";
+                    }
                 }
-            }, 10)// Je mets un settimeout pour éviter de récupérer l'input check trop vite. Ca permet de valider le check et ensuite de le récupérer. Sinon ça exec trop vite.
-        })
+            }, 10);// Je mets un settimeout pour éviter de récupérer l'input check trop vite. Ca permet de valider le check et ensuite de le récupérer. Sinon ça exec trop vite.
+        });
 
     }
-})
+});
 
-//SEARCH INPUT
-const searchInput = document.querySelector("#search");
-const tbody = document.querySelector("tbody");
-const thead = document.querySelector("thead")
-const table = document.querySelector("table")
-const resultMsg = document.querySelector("#search-result");
+searchInput.addEventListener("input", () => {filterData(city);});
 
-searchInput.addEventListener("input", filterData)
-let streetInterval = [];
-let street = [];
-let sectorColor = [];
-let negoName = [];
-
-function filterData() {
-    const searchedString = searchInput.value.toLowerCase()
-    if (searchInput.value.length >= 3) {
-        for (i = 0; i < dataArray.length; i++) {
-            if (dataArray[i].streetName.match(searchedString)) {
-                streetInterval.push(dataArray[i].streetInfo.interval)
-                street.push(dataArray[i].streetName)
-                sectorColor.push(dataArray[i].streetInfo.sector)
-                negoName.push(dataArray[i].streetInfo.nego)
+async function filterData(cityName) {
+    const searchedString = searchInput.value.toLowerCase();
+    console.log(searchedString);
+    if (searchedString.length >= 3){
+        try {
+            const response = await fetch('./data/'+ cityName +'.json');
+            if (!response.ok) {
+                throw new Error('Erreur lors du chargement du fichier JSON');
             }
+            const data = await response.json();
+            const result = data.filter(city => city.streetName.includes(searchedString));
+
+            if(result.length){
+                resultMsg.textContent = "Nombre de résultat : " + result.length;
+                table.style.opacity = "1";
+                thead.textContent = "";
+                tbody.textContent = "";
+                createTr(result);
+            } else {
+                clearTable();
+            }
+        } catch (error) {
+            console.error('Une erreur s\'est produite lors du chargement du fichier JSON', error);
         }
-        
-        if (street.length) {
-            resultMsg.textContent = "Nombre de résultat : " + streetInterval.length;
-            table.style.opacity = "1";
-            thead.textContent = "";
-            tbody.textContent = "";
-            createTr(streetInterval, street, sectorColor, negoName);
-        } else {
-            resultMsg.textContent = "Aucun résultat"
-            table.style.opacity = "0";
-            thead.textContent = "";
-            tbody.textContent = ""
-        }
-    
-        streetInterval = [];
-        street = [];
-        sectorColor = [];
-        negoName = [];
-    
+
     } else {
-        thead.textContent = "";
-        tbody.textContent = "";
-        resultMsg.textContent = "";
-        table.style.opacity = "0";
+        clearTable();
     }
 }
 
 //FILL TABLE
-function createTr(streetInterval, street, sectorColor, negoName) {
+function createTr(obj) {
+// function createTr(streetInterval, street, sectorColor, negoName) {
     thead.insertAdjacentHTML("afterbegin", `
         <tr>
             <th>Interval</th>
@@ -100,32 +92,39 @@ function createTr(streetInterval, street, sectorColor, negoName) {
             <th>Négociateur</th>
             <th>Google Map</th>
         </tr>
-        `)
+        `);
+    obj.forEach(result => {
 
-    for (i = 0; i<street.length; i++){
+
+
         tbody.insertAdjacentHTML("afterbegin", `
         <tr>
-            <td>${streetInterval[i]}</td>
-            <td>${street[i]}</td>
+            <td>${result.streetInfo.interval}</td>
+            <td>${result.streetName}</td>
             <td>${city}</td>
-            <td id="sector-color">${sectorColor[i]}</td>
-            <td>${negoName[i]}</td>
-            <td><a href="https://www.google.fr/maps/place/${street[i]}+${city}" target="_blank">Visualiser</a></td>
+            <td id="sector-color" data-color="${result.streetInfo.sector}"></td>
+            <td>${result.streetInfo.nego}</td>
+            <td><a href="https://www.google.fr/maps/place/${result.streetName}+${city}" target="_blank">Visualiser</a></td>
         </tr>
-        `)
+        `);
 
-        const changeSectorColor = document.querySelector("#sector-color")
-        if (sectorColor[i] === "Vert") {
-            changeSectorColor.style.background = "rgb(6, 163, 6)"
-        } else if (sectorColor[i] === "Jaune") {
-            changeSectorColor.style.background = "rgb(223, 223, 22)"
-            changeSectorColor.style.color = "black"
-        } else if (sectorColor[i] === "Rose") {
-            changeSectorColor.style.background = "rgb(207, 144, 154)"
-        } else if (sectorColor[i] === "Orange"){
-            changeSectorColor.style.background = "orange"
+        const changeSectorColor = document.querySelector("#sector-color");
+        if (changeSectorColor.getAttribute("data-color") === "Vert") {
+            changeSectorColor.style.background = "rgb(6, 163, 6)";
+        } else if (changeSectorColor.getAttribute("data-color") === "Jaune") {
+            changeSectorColor.style.background = "rgb(223, 223, 22)";
+            changeSectorColor.style.color = "black";
+        } else if (changeSectorColor.getAttribute("data-color") === "Rose") {
+            changeSectorColor.style.background = "rgb(207, 144, 154)";
+        } else if (changeSectorColor.getAttribute("data-color") === "Orange"){
+            changeSectorColor.style.background = "orange";
         }
-    }
+    });
 }
 
-// https://www.google.fr/maps/place/5+Av.+Ch%C3%A2teaubriand,+95190+Goussainville/
+function clearTable(){
+    thead.textContent = "";
+    tbody.textContent = "";
+    resultMsg.textContent = "";
+    table.style.opacity = "0";
+}
